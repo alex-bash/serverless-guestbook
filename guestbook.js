40f336e0c8a6b33b6bad1b8ecb48d6095617a974
alex-bash <59192212+alex-bash@users.noreply.github.com>
2022-02-01 15:12:38 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://4551154f.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
